09445a38c5420fa590c34a89d3b1365e5ae9d930
Night-Soft <yurafisher2@gmail.com>
2026-03-06 23:27:31 +0200

    feat: lambda with capture

--- src/esp/wifi/PostponedTask.cpp
+++ src/esp/wifi/PostponedTask.cpp
@@ -1,69 +1,130 @@
 #include "PostponedTask.h"
 
-//using Callback = std::function<void()>;
-using Callbacks = std::vector<Callback>;
-using Tasks = std::vector<Task>;
-
 PostponedTask::PostponedTask() {}
 PostponedTask::~PostponedTask() {}
 
-//template <typename Cb>
-void PostponedTask::add(uint32 timeMs, void(*callback)(), bool once) {
-  if(this->has(callback)) {
+uint32 PostponedTask::add(uint32 timeMs, Callback callback, bool once) {
+  Serial.println("Лямбда БЕЗ захвата");
+
+  if (this->has(callback)) {
     Serial.println("PostponedTask: callback already exsists.");
-    return;
+    return (uint32)callback;
   };
-  this->_size++;
-  events.emplace(
-      callback,
-      Task{.once = once, .delay = timeMs, .timer = (uint32)millis(), .callback = callback});
+
+  events.emplace(callback, Task{.once = once,
+                                .delay = timeMs,
+                                .timer = (uint32)millis(),
+                                .callback = callback});
+
+  this->ids.emplace_back((uint32)callback);
+  this->updateSize();
+  return (uint32)callback;
+}
+
+uint32 PostponedTask::add(uint32 timeMs, CallbackLm callback, bool once) {
+    Serial.println("Лямбда с захватом");
+
+    const uint32 id = this->taskId++;
+    eventsLm.emplace(id, TaskLm{.once = once,
+                                     .delay = timeMs,
+                                     .timer = (uint32)millis(),
+                                     .callback = callback});
+
+    this->ids.emplace_back(id);
+    this->updateSize();
+    return id;
 }
 
 bool PostponedTask::has(Callback callback) {
   return events.find(callback) != events.end();
 }
 
+bool PostponedTask::has(uint32 id) {
+  return eventsLm.find(id) != eventsLm.end();
+}
+
 bool PostponedTask::remove(Callback callback) {
   if (this->has(callback) == false) return false;
   events.erase(callback);
-  this->_size--;
+  this->updateSize();
   return true;
 }
 
-size_t PostponedTask::size() { return (size_t)this->_size; }
+bool PostponedTask::remove(uint32 id) {
+  if (this->has(id) == false) return false;
+  eventsLm.erase(id);
+  this->updateSize();
+  return true;
+}
 
-template <typename F>
-void PostponedTask::forEach(F callback) {
-  bool needBreak = false;
+void PostponedTask::removeList(Callbacks &list) {
+  for (const auto& cb : list) {
+    events.erase(cb);
+  }
+  this->updateSize();
+}
+void PostponedTask::removeList(CallbacksIds &list) {
+  Callback ptr;
+  for (const auto& id : list) {
+    // erase id
+    auto itIds = std::find(ids.begin(), ids.end(), id);
+    if(itIds != ids.end()) ids.erase(itIds);
+    // erase usual fn
+    ptr = (Callback)(id);
+    auto it = events.find(ptr);
+    if(it != events.end()) {
+      events.erase(ptr);
+      continue;
+    }
+    // erase lamda
+    auto itLm = eventsLm.find(id);
+    if(itLm != eventsLm.end()) {
+      eventsLm.erase(itLm);
+    }
+  }
+  this->updateSize();
+}
 
-  for (const auto& pair : events) {
-    if (needBreak == true) break;
+void PostponedTask::updateSize() {
+  this->size = (uint8_t)(events.size() + eventsLm.size());
+}
 
-    callback((Task&)pair.second, (Callback&)pair.second.callback, needBreak);
+// template <typename F>
+// void PostponedTask::forEach(F callback) {
+//   bool needBreak = false;
+
+//   for (const auto& pair : events) {
+//     if (needBreak == true) break;
+
+//     callback((Task&)pair.second, (Callback&)pair.second.callback, needBreak);
+//   }
+// }
+
+void PostponedTask::setMinTickTime(const uint32& delay, const uint32& timer,
+                                     const uint32& now, uint32& minTime) {
+  if (delay < minTime) minTime = delay;
+
+  int timeLeftNextCall = delay - (now - timer);  // (timePassed)
+  if (timeLeftNextCall >= 0 && timeLeftNextCall < (int)minTime) {
+    minTime = timeLeftNextCall;
   }
 }
 
 void PostponedTask::updateMinTickTime() {  // setMinTickTime
-  if (this->_size == 0) return;
+  if (this->size == 0) return;
 
   uint32 now = millis(); 
-  uint32 minTime = events.begin()->second.delay;
+  uint32 minTime = 0xffffffff;
 
-  this->forEach(
-      [&minTime, &now](Task& task, Callback& cb, bool& needBreak) {
-        if (task.delay < minTime) minTime = task.delay;
+  for(const auto&[cb, task] : events) setMinTickTime(task.delay, task.timer, now, minTime);
+  for(const auto&[id, task] : eventsLm) setMinTickTime(task.delay, task.timer, now, minTime);
 
-        int timeLeftNextCall = task.delay - (now - task.timer); // (timePassed)
-        if (timeLeftNextCall >= 0 && timeLeftNextCall < (int)minTime) {
-          minTime = timeLeftNextCall;
-        }
-      });
-  //Serial.printf("minTickTime: %d, minTime: %d\n", minTickTime, minTime);
+  Serial.printf("minTickTime: %d, minTime: %d\n", minTickTime, minTime);
   minTickTime = minTime;
 }
 
-void PostponedTask::tick() {
-  if (this->_size == 0) return;
+void PostponedTask::tick() { 
+  if (this->size == 0) return;
   
   static uint32 timer = 0;
   uint32 now = millis();
@@ -71,39 +132,39 @@ void PostponedTask::tick() {
   if(now - timer < this->minTickTime) return;
   timer = now;
 
-  Callbacks cbToErase;
+  CallbacksIds idToErase;
 
-  this->forEach([&cbToErase, &now](Task& task, Callback& cb, bool& needBreak) {
-    if (now - task.timer >= task.delay) {  // call all time passed callbacks // todo
-      task.timer = now; // todo
-      cb();
-
-      if (task.once == true) {
-        cbToErase.emplace_back(cb);
+  for (const auto& id : ids) {
+    Callback ptr = (Callback)(id); 
+    auto itEvents = events.find(ptr);
+    if (itEvents != events.end()) {
+      Task& task = itEvents->second;
+      if (now - task.timer >= task.delay) {
+        task.timer = now;
+        task.callback();
+        if (task.once == true) {
+          idToErase.emplace_back(id);
+        }
       }
+      continue;
     }
-  });
 
-  for(const auto& cb : cbToErase) {
-    events.erase(cb);
-    this->_size--;
+    auto itEventsLm = eventsLm.find(id);
+    if (itEventsLm != eventsLm.end()) {
+      TaskLm& task = itEventsLm->second;
+      if (now - task.timer >= task.delay) {
+        task.timer = now;
+        task.callback(id);
+        if (task.once == true) {
+          idToErase.emplace_back(id);
+        }
+      }
+      continue;
+    }
   }
-
+  
+  removeList(idToErase);
   this->updateMinTickTime();
 }
 
 PostponedTask postponedTask;
-
-    // // Лямбда без захвата — конвертируется в указатель на функцию?
-    // constexpr bool isConvertible = std::is_convertible_v<Cb, void(*)()>;
-
-    // // Лямбда с захватом — НЕ конвертируется
-    // constexpr bool hasCapture = std::is_class_v<Cb> && !isConvertible;
-
-    // if constexpr (isConvertible && std::is_class_v<Cb>) {
-    //     Serial.println("Лямбда БЕЗ захвата");
-    // } else if constexpr (hasCapture) {
-    //     Serial.println("Лямбда С захватом");
-    // } else {
-    //     Serial.println("Обычная функция");
-    // }
--- src/esp/wifi/PostponedTask.h
+++ src/esp/wifi/PostponedTask.h
@@ -5,14 +5,26 @@
 #include <functional>
 #include <map>
 
+// Callback for usual function
 using Callback = void(*)();
+// Callback for lambda function
+using CallbackLm = std::function<void(uint32)>;
+
+using Callbacks = std::vector<Callback>;
+using CallbacksIds = std::vector<uint32>;
 
 struct Task {
   bool once;
   uint32 delay;
   uint32 timer;
   Callback callback;
-  // Callback calback;
+};
+
+struct TaskLm {
+  bool once;
+  uint32 delay;
+  uint32 timer;
+  CallbackLm callback;
 };
 
 using CbForEach = void(*)(Task&, Callback&, bool&); // value , key, needBreak
@@ -23,23 +35,32 @@ class PostponedTask {
   PostponedTask();
   ~PostponedTask();
 
-//  template <typename Cb>
-  void add(uint32 timeMs, Callback callback, bool once = true);
+  uint32 add(uint32 timeMs, Callback callback, bool once = true);
+  uint32 add(uint32 timeMs, CallbackLm callback, bool once = true);
 
   void tick();
 
   bool has(Callback callback);
+  bool has(uint32 id);
   bool remove(Callback callback);
-  size_t size();
+  bool remove(uint32 id);
+  uint8_t size;
 
-  template <typename F>
-  void forEach(F Callback);
+  // template <typename F>
+  // void forEach(F Callback);
 
  private:
-  uint8_t _size = 0;
+  CallbacksIds ids;
+  uint32 taskId = 1;
   uint32 minTickTime = 1000;
+  void removeList(Callbacks &list);
+  void removeList(CallbacksIds &list);
+  void updateSize();
   void updateMinTickTime();
-  std::map<void(*)(), Task> events;
+  void setMinTickTime(const uint32& delay, const uint32& timer,
+                        const uint32& now, uint32& minTime);
+  std::map<Callback, Task> events;
+  std::map<uint32, TaskLm> eventsLm;
 };
 
 extern PostponedTask postponedTask;
