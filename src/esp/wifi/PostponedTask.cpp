815b2ab2e47496a9bbf79f1da84bc09d0a33389f
Night-Soft <yurafisher2@gmail.com>
2026-03-16 13:22:03 +0200

    improved tasker

--- src/esp/wifi/PostponedTask.cpp
+++ src/esp/wifi/PostponedTask.cpp
@@ -1,104 +1,471 @@
 #include "PostponedTask.h"
 
-PostponedTask::PostponedTask() {}
-PostponedTask::~PostponedTask() {}
+TASK_TYPE getTaskType(uint8 type) {
+  TASK_TYPE t;
+  switch (type) {
+    case 1:
+      t = TASK_TYPE::regural;
+      break;
+    case 2:
+      t = TASK_TYPE::lambda;
+      break;
+    default:
+      t = TASK_TYPE::empty;
+      break;
+  }
+  return t;
+}
+
+Ids::Ids(uint8 maxIds){
+  size_t sizePtr = sizeof(void*);
+  size_t sizeId = sizeof(uint8);
+  size_t sizeType = sizeof(uint8);
+  size_t sizeOneEl = sizePtr + sizeId + sizeType;
+  itemSize = sizeOneEl;
+  ids = (uint8*)calloc(sizeOneEl, maxIds);
+}
+
+Ids::~Ids() {
+  if (ids != nullptr) free(ids);
+}
+
+void Ids::add(CallbackId id, void* ptr, TASK_TYPE type) {
+  if(has(id)) remove(id); // move callback to end of list
+
+  Serial.printf("Add id: %d\n", id);
+
+  //uint8 *ptrTask = (uint8*)&ptr;
+  uint8 *ptrIds = ids + (size * itemSize);
+
+  ptrIds[0] = id;
+  ptrIds[1] = (uint8)type;
+  memcpy(ptrIds + 2, &ptr, sizeof(ptr)); 
+  size++;
+}
+
+void Ids::remove(CallbackId id) {
+  uint8* ptr = ids;
+  Serial.printf("Remove id: %d\n", id);
+
+  for (uint8 i = 0; i < size; i++) {
+    if (*ptr == id) {
+      shiftMem(ptr);
+      break;
+    }
+    ptr += itemSize;
+  }
+}
+
+void Ids::remove(uint8* taskPtr) {
+  uint8* ptrIds = ids;
+  uint8*restored = nullptr; // ptr to task in tasks
+
+  for (uint8 i = 0; i < size; i++) {
+    memcpy(&restored, ptrIds + 2, sizeof(void*)); //create ptr to task in tasks from ids
+    ptrIds += itemSize;
+    if (taskPtr != restored) continue;
+
+    shiftMem(ptrIds - itemSize);
+    return;
+  }
+}
+
+bool Ids::has(const CallbackId& id) {
+  uint8 *ptr = ids;
+
+  for(uint8 i = 0; i < size; i++) {
+    if(*ptr == id) return true;
+    ptr += itemSize;
+  }
+
+  return false;
+}
+
+TaskPT Ids::get(const CallbackId& id){
+  uint8 *ptr = ids;
+  TaskPT taskPt{.pointer = nullptr, .type = TASK_TYPE::empty};
+
+  for(uint8 i = 0; i < size; i++) {
+    if(*ptr == id) {
+      memcpy(&taskPt.pointer, ptr + 2, sizeof(void*));
+      taskPt.type = (TASK_TYPE)*(ptr + 1);
+      return taskPt;
+    }
+    ptr += itemSize;
+  }
+
+  return taskPt;
+}
+
+//ptr[0] = id, ptr[1] = TASK_TYPE, ptr[2] = ptr  
+TaskPT Ids::get(uint8* ptr) {
+  uint8 *ptrIds = ids;
+  TaskPT taskPt{.pointer = nullptr, .type = TASK_TYPE::empty};
+  uint8* resotored = nullptr;
+
+  for(uint8 i = 0; i < size; i++) {
+    memcpy(&resotored, ptrIds + 2, sizeof(void*));
+    ptrIds += itemSize;
+    if(ptr != resotored) continue;
+
+    taskPt.pointer = resotored;
+    taskPt.type = (TASK_TYPE)(ptrIds - itemSize)[1];
+    return taskPt;
+    }
+
+  return taskPt;
+}
+
+bool Ids::updateNumTasks(uint8 maxRegural, uint8 maxLambda) {
+  uint8* reallocPtr = (uint8*)realloc(ids, itemSize * (maxRegural + maxLambda));
+  if(reallocPtr == nullptr) return false;
+  ids = reallocPtr;
+  return true;
+}
+
+void Ids::shiftMem(uint8* dest) {
+  uint8* source = dest + itemSize;
+  uint8* endMemPtr = ids + (size * itemSize);
+  size_t sizeCpy = endMemPtr - source;
+
+  if (sizeCpy > 0) memcpy(dest, source, sizeCpy);  // dest, source, size
+  size--;
+}
+
+
+Tasks::Tasks(uint8 maxRegural, uint8 maxLambda)
+    : ids(maxRegural + maxLambda) {
+  size_t sizeRegural = sizeof(Task) * maxRegural;
+  size_t sizeLambda = sizeof(TaskLm) * maxLambda;
+  maxSizeTasksInBytes = sizeRegural* maxRegural + sizeLambda * maxLambda; 
+  beginTasks = (uint8*)calloc(sizeRegural + sizeLambda, sizeof(uint8));
+  endTasks = beginTasks;
+}
+
+Tasks::~Tasks() { free(beginTasks); }
+
+void Tasks::add(Task& task) {
+  // if (isFreeMemForTask(sizeof(Task)) == false && needDefragmentaton()) {
+  //   defragmentation();
+  // }
+
+  memcpy(endTasks, &task, sizeof(Task));  // dest, source, len
+  ids.add(task.id, endTasks, TASK_TYPE::regural);
+
+  sizeTasksInBytes += sizeof(Task);
+  endTasks+= sizeof(Task);
+  size++;
+}
+
+void Tasks::add(TaskLm& task) {
+
+  memcpy(endTasks, &task, sizeof(TaskLm));  // dest, source, len
+  ids.add(task.id, endTasks, TASK_TYPE::lambda);
+
+  sizeTasksInBytes += sizeof(TaskLm);
+  endTasks+= sizeof(TaskLm);
+  size++;
+}
+
+void Tasks::remove(CallbackId id) {
+  TaskPT taskPt = this->get(id);
+  remove(taskPt);
+}
+
+void Tasks::remove(TaskPT& taskPt) {
+  CallbackId id = 0;
+  if (taskPt.type == TASK_TYPE::regural) {
+    id = ((Task*)taskPt.pointer)->id;
+    sizeTasksInBytes -= sizeof(Task);
+  } else if (taskPt.type == TASK_TYPE::lambda) {
+    id = ((TaskLm*)taskPt.pointer)->id;
+    sizeTasksInBytes -= sizeof(TaskLm);
+  } else {
+    Serial.printf("Error remove taskPt, unknown type: %d\n", (int)taskPt.type);
+    return;
+  }
+
+  ids.remove(id); // todo return bool  
+}
+
+void Tasks::removeList(TaskPT taskPt[], uint8 len) {
+  for (uint8 i = 0; i < len; i++) {
+    remove(taskPt[i]);
+  }
+}
+
+bool Tasks::has(const CallbackId& id) { return this->has(id); }
+
+bool Tasks::has(Callback callback) {
+  TaskPT taskPT = this->get(callback);
+  if (taskPT.type == TASK_TYPE::regural) return true;
+  return false;
+}
+
+TaskPT Tasks::get(const CallbackId& id) { return ids.get(id); }
+
+TaskPT Tasks::get(Callback callback) { // remove for , try in ids getCallback
+  if (this->size == 0) return TaskPT();
+
+  Task *task = nullptr;
+  for (const TaskPT&taskPt : *this) {
+    if(taskPt.type != TASK_TYPE::regural) continue;
+    task = (Task*)taskPt.pointer;
+    if(task->callback == callback) return taskPt;
+  }
+
+  return TaskPT();
+}
+
+//memcpy(buffer, ptr, sizeof(ptr)); копировать данные из объекта value
+//memcpy(buffer, &ptr, sizeof(ptr)); копировать байты из памяти начиная с 0x, (копия value of ptr )
+void Tasks::defragmentation() {
+  Serial.println("Start defragmentation");
+
+  uint8* curTask = nullptr;  // curTask is in tasks
+  uint8* tasksPtr = beginTasks;
+  uint8* ptrIds = ids.ids;
+  uint8 shiftSize = 0;
+
+  for(int i = 0; i < ids.size; i++) { 
+    memcpy(&curTask, ptrIds + 2, sizeof(void*));
+
+    TASK_TYPE type = getTaskType((uint8) * (ptrIds + 1));
+
+    if(type == TASK_TYPE::regural) {
+      shiftSize = sizeof(Task);
+    } else if (type == TASK_TYPE::lambda) {
+      shiftSize = sizeof(TaskLm);
+    } else {
+      Serial.println("Error defragmentation");
+      return;
+    }
+
+    // copy the task to the beginning of memory
+    memcpy(tasksPtr, curTask, shiftSize);
+
+    // update pointer to task in ids
+    memcpy(ptrIds + 2, &tasksPtr, sizeof(void*));
+
+    ptrIds += ids.itemSize;
+    tasksPtr += shiftSize;
+  }
+
+  endTasks = tasksPtr;
+}
+
+bool Tasks::needDefragmentaton(uint8 taskSize){
+  return (!canAddTask(sizeof(TaskLm)) && isFreeMemForTask(sizeof(TaskLm)));
+}
+
+bool Tasks::canAddTask(uint8 taskSize) {
+  return endTasks + taskSize < beginTasks + maxSizeTasksInBytes;
+}
+
+bool Tasks::isFreeMemForTask(uint8 taskSize) {
+  return sizeTasksInBytes + taskSize <= this->maxSizeTasksInBytes;
+}
+
+bool Tasks::updateNumTasks(uint8 maxRegural, uint8 maxLambda) {
+  if (ids.updateNumTasks(maxRegural, maxLambda) == false) return false;
+  defragmentation();
+
+  size_t sizeRegural = sizeof(Task) * maxRegural;
+  size_t sizeLambda = sizeof(TaskLm) * maxLambda;
+  maxSizeTasksInBytes = sizeRegural* maxRegural + sizeLambda * maxLambda; 
+  uint8* newTasksPtr = (uint8*)realloc(beginTasks, sizeRegural + sizeLambda);
+
+  if(newTasksPtr == nullptr) return false;
+  if(newTasksPtr == beginTasks) return true;
+  beginTasks = newTasksPtr;
+  endTasks = beginTasks + sizeTasksInBytes;
+  // updata tasks pointers in ids
+  uint8* curTaskPtr = beginTasks;
+  uint8* ptrIds = ids.ids;
+  uint8 shiftSize = 0;
+  for(uint8 i = 0; i < size; i++) { // todo check
+    TASK_TYPE type = getTaskType((uint8)*(ptrIds + 1));
+
+    if(type == TASK_TYPE::regural) {
+      shiftSize = sizeof(Task);
+    } else if (type == TASK_TYPE::lambda) {
+      shiftSize = sizeof(TaskLm);
+    } else {
+      Serial.println("Error updateNumTasks");
+      return false;
+    }
+    //copy pointer of cur task to ids
+    memcpy(ptrIds + 2, &curTaskPtr, sizeof(void*));
+    curTaskPtr += shiftSize;
+    ptrIds += ids.itemSize;
+  }
+
+  return true;
+}
+
+TaskIterator Tasks::begin() {
+  return TaskIterator(ids.ids, ids.itemSize, size);
+}  // always return begin
+
+TaskIterator Tasks::end() {
+  return TaskIterator(ids.ids + (ids.itemSize * ids.size));
+}  // always return end element
+
+PostponedTask::PostponedTask()
+    : tasks(MAX_REGURAL_TASK, MAX_LAMBDA_TASK), size(tasks.size) {
+  taskToErase = (TaskPT*)calloc(MAX_REGURAL_TASK + MAX_LAMBDA_TASK, sizeof(TaskPT));
+}
+
+PostponedTask::PostponedTask(uint8 maxRegural, uint8 maxLambda)
+    : tasks(maxRegural, maxLambda), size(tasks.size) {
+  taskToErase = (TaskPT*)calloc(maxRegural + maxLambda, sizeof(TaskPT));
+}
+
+PostponedTask::~PostponedTask() {
+  if(taskToErase != nullptr) free(taskToErase);
+}
 
 uint32 PostponedTask::add(uint32 timeMs, Callback callback, bool once) {
-  Serial.println("Лямбда БЕЗ захвата");
+  if (tasks.needDefragmentaton(sizeof(Task))) {
+    tasks.defragmentation();
+
+    if (tasks.canAddTask(sizeof(Task)) == false) {
+      Serial.println("Can`t add task, limit of memory!");
+      return 0;
+    }
+  }
 
   if (this->has(callback)) {
     Serial.println("PostponedTask: callback already exsists.");
-    return (uint32)callback;
+    return 0;
   };
 
-  events.emplace(callback, Task{.once = once,
-                                .delay = timeMs,
-                                .timer = (uint32)millis(),
-                                .callback = callback});
+  const CallbackId id = getTaskId(TASK_TYPE::regural); 
+  Task task = {.once = once,
+               .id = id,
+               .delay = timeMs,
+               .timer = (uint32)millis(),
+               .callback = callback};
 
-  this->ids.emplace_back((uint32)callback);
-  this->updateSize();
-  return (uint32)callback;
+  Serial.printf("Add regural -> id: %d, delay: %d\n", task.id, task.delay);
+
+  tasks.add(task);
+  return (uint32)id;
 }
 
 uint32 PostponedTask::add(uint32 timeMs, CallbackLm callback, bool once) {
-    Serial.println("Лямбда с захватом");
+  if (tasks.needDefragmentaton(sizeof(TaskLm))) {
+    tasks.defragmentation();
 
-    const uint32 id = this->taskId++;
-    eventsLm.emplace(id, TaskLm{.once = once,
-                                     .delay = timeMs,
-                                     .timer = (uint32)millis(),
-                                     .callback = callback});
+    if (tasks.canAddTask(sizeof(TaskLm)) == false) {
+      Serial.println("Can`t add taskLm, limit of memory!");
+      return 0;
+    }
+  }
 
-    this->ids.emplace_back(id);
-    this->updateSize();
-    return id;
-}
+  const CallbackId id = getTaskId(TASK_TYPE::lambda);
+  TaskLm task = {.once = once,
+                 .id = id,
+                 .delay = timeMs,
+                 .timer = (uint32)millis(),
+                 .callback = callback};
 
-bool PostponedTask::has(Callback callback) {
-  return events.find(callback) != events.end();
-}
+  Serial.printf("Add lambda -> id: %d, delay: %d\n", task.id, task.delay);
 
-bool PostponedTask::has(uint32 id) {
-  return eventsLm.find(id) != eventsLm.end();
+  tasks.add(task);
+  return id;
 }
 
 bool PostponedTask::remove(Callback callback) {
-  if (this->has(callback) == false) return false;
-  events.erase(callback);
-  this->updateSize();
+  TaskPT taskPT = tasks.get(callback);
+  if(taskPT.pointer == nullptr) return false;
+  tasks.remove(taskPT);
   return true;
 }
 
-bool PostponedTask::remove(uint32 id) {
-  if (this->has(id) == false) return false;
-  eventsLm.erase(id);
-  this->updateSize();
+bool PostponedTask::remove(CallbackId id) {
+  tasks.remove(id);
   return true;
 }
 
-void PostponedTask::removeList(Callbacks &list) {
-  for (const auto& cb : list) {
-    events.erase(cb);
-  }
-  this->updateSize();
-}
-void PostponedTask::removeList(CallbacksIds &list) {
-  Callback ptr;
-  for (const auto& id : list) {
-    // erase id
-    auto itIds = std::find(ids.begin(), ids.end(), id);
-    if(itIds != ids.end()) ids.erase(itIds);
-    // erase usual fn
-    ptr = (Callback)(id);
-    auto it = events.find(ptr);
-    if(it != events.end()) {
-      events.erase(ptr);
+bool PostponedTask::has(Callback callback) { return tasks.has(callback); }
+
+bool PostponedTask::has(CallbackId id) { return tasks.has(id); }
+
+void PostponedTask::tick() { 
+  if (this->size == 0) return;
+
+  static uint32 timer = 0;
+  uint32 now = millis();
+
+  if(now - timer < this->minTickTime) return;
+  timer = now;
+
+  numToErase = 0; // todo remove inside
+
+  //uint64 prev = micros64();
+
+  for (const TaskPT& taskPt : this->tasks) {
+    if (taskPt.type == TASK_TYPE::empty) {
+      Serial.println("Task type error, empty");
+      break;
+    }
+
+    if (taskPt.type == TASK_TYPE::regural) {
+      //Serial.println("In TASK_TYPE::regural");
+
+      regural = (Task*)taskPt.pointer;
+      if (now - regural->timer >= regural->delay) {
+        if (regural->once == true) taskToErase[numToErase++] = taskPt;
+        regural->timer = now;
+        regural->callback();
+      }
       continue;
     }
-    // erase lamda
-    auto itLm = eventsLm.find(id);
-    if(itLm != eventsLm.end()) {
-      eventsLm.erase(itLm);
+
+    lambda = (TaskLm*)taskPt.pointer;
+    if (now - lambda->timer >= lambda->delay) {
+     // Serial.println("In TASK_TYPE::lambda");
+
+      if (lambda->once == true) taskToErase[numToErase++] = taskPt;
+      lambda->timer = now;
+      lambda->callback(lambda->id);
     }
   }
-  this->updateSize();
+
+  if (numToErase) {
+    //Serial.println("try removeList");
+    tasks.removeList(this->taskToErase, numToErase);
+  }
+  this->updateMinTickTime();
+
+ // Serial.printf("BeetwenTick: %llu micros\n",(uint64)(micros64() - prev));
+
 }
 
-void PostponedTask::updateSize() {
-  this->size = (uint8_t)(events.size() + eventsLm.size());
+bool PostponedTask::updateNumTasks(uint8 maxRegural, uint8 maxLambda) { 
+  if(!tasks.updateNumTasks(maxRegural, maxLambda)) return false;
+  // todo improve log and realloc
+  TaskPT* newPtr = (TaskPT*)realloc(taskToErase,(maxRegural + maxLambda) * sizeof(TaskPT));
+  if(newPtr == nullptr) return false;
+  return true;
 }
 
-// template <typename F>
-// void PostponedTask::forEach(F callback) {
-//   bool needBreak = false;
+void PostponedTask::removeList(Callbacks& list, uint8 len) {
+  TaskPT taskPT;
 
-//   for (const auto& pair : events) {
-//     if (needBreak == true) break;
+  for (uint8 i = 0; i < len; i++) {
+    taskPT = tasks.get(list[i]);
+    if (taskPT.pointer == nullptr) continue;
+    tasks.remove(taskPT);
+  }
+}
 
-//     callback((Task&)pair.second, (Callback&)pair.second.callback, needBreak);
-//   }
-// }
+void PostponedTask::removeList(CallbacksIds &list, uint8 len) {
+  for (uint8 i = 0; i < len; i++) {
+    tasks.remove(list[i]);
+  }
+}
 
 void PostponedTask::setMinTickTime(const uint32& delay, const uint32& timer,
                                      const uint32& now, uint32& minTime) {
@@ -114,57 +481,39 @@ void PostponedTask::updateMinTickTime() {  // setMinTickTime
   if (this->size == 0) return;
 
   uint32 now = millis(); 
-  uint32 minTime = 0xffffffff;
+  uint32 minTime = 0xffffffff; //max
 
-  for(const auto&[cb, task] : events) setMinTickTime(task.delay, task.timer, now, minTime);
-  for(const auto&[id, task] : eventsLm) setMinTickTime(task.delay, task.timer, now, minTime);
+  Task *regural;
+  TaskLm *lambda;
+  for (const TaskPT& taskPt : this->tasks) {
+    if(taskPt.type == TASK_TYPE::empty) {
+      Serial.println("Task type error, empty");
+      continue;
+    }
 
-  Serial.printf("minTickTime: %d, minTime: %d\n", minTickTime, minTime);
+    if(taskPt.type == TASK_TYPE::regural) {
+      regural = (Task*)taskPt.pointer;
+      setMinTickTime(regural->delay, regural->timer, now, minTime);
+      continue;
+    }
+    lambda = (TaskLm*)taskPt.pointer;
+    setMinTickTime(lambda->delay, lambda->timer, now, minTime);
+  }
+
+  //Serial.printf("minTickTime: %d, minTime: %d\n", minTickTime, minTime);
   minTickTime = minTime;
 }
 
-void PostponedTask::tick() { 
-  if (this->size == 0) return;
-  
-  static uint32 timer = 0;
-  uint32 now = millis();
-
-  if(now - timer < this->minTickTime) return;
-  timer = now;
-
-  CallbacksIds idToErase;
-
-  for (const auto& id : ids) {
-    Callback ptr = (Callback)(id); 
-    auto itEvents = events.find(ptr);
-    if (itEvents != events.end()) {
-      Task& task = itEvents->second;
-      if (now - task.timer >= task.delay) {
-        task.timer = now;
-        task.callback();
-        if (task.once == true) {
-          idToErase.emplace_back(id);
-        }
-      }
-      continue;
-    }
-
-    auto itEventsLm = eventsLm.find(id);
-    if (itEventsLm != eventsLm.end()) {
-      TaskLm& task = itEventsLm->second;
-      if (now - task.timer >= task.delay) {
-        task.timer = now;
-        task.callback(id);
-        if (task.once == true) {
-          idToErase.emplace_back(id);
-        }
-      }
-      continue;
-    }
+CallbackId& PostponedTask::getTaskId(TASK_TYPE type) {
+  if (type == TASK_TYPE::regural) {
+    if (taskId == 127) taskId = 26;
+    return ++taskId;
   }
-  
-  removeList(idToErase);
-  this->updateMinTickTime();
+  if (type == TASK_TYPE::lambda) {
+    if (taskLmId == 255) taskLmId = 154;
+    return ++taskLmId;
+  }
+  return numToErase; // todo
 }
 
 PostponedTask postponedTask;
--- src/esp/wifi/PostponedTask.h
+++ src/esp/wifi/PostponedTask.h
@@ -2,19 +2,25 @@
 #define POST_PONED_TASK_H
 
 #include <Arduino.h>
+
 #include <functional>
 #include <map>
 
+#define MAX_REGURAL_TASK 32
+#define MAX_LAMBDA_TASK 16
+
 // Callback for usual function
-using Callback = void(*)();
+using Callback = void (*)();
 // Callback for lambda function
 using CallbackLm = std::function<void(uint32)>;
 
-using Callbacks = std::vector<Callback>;
-using CallbacksIds = std::vector<uint32>;
+using Callbacks = Callback*;
+using CallbacksIds = uint8*;
+using CallbackId = uint8;
 
 struct Task {
   bool once;
+  CallbackId id;
   uint32 delay;
   uint32 timer;
   Callback callback;
@@ -22,45 +28,149 @@ struct Task {
 
 struct TaskLm {
   bool once;
+  CallbackId id;
   uint32 delay;
   uint32 timer;
   CallbackLm callback;
 };
 
-using CbForEach = void(*)(Task&, Callback&, bool&); // value , key, needBreak
+using CbForEach = void (*)(Task&, Callback&, bool&);  // value , key, needBreak
 
+enum class TASK_TYPE : uint8 { empty = 0, regural = 1, lambda = 2 };
 
-class PostponedTask {
+TASK_TYPE getTaskType(uint8 type);
+
+struct TaskPT { //pointer to Task and type
+  void* pointer = nullptr;
+  TASK_TYPE type = TASK_TYPE::empty;
+};
+
+class Ids {
+ public:
+  Ids(uint8 maxIds);
+  ~Ids();
+
+  void add(CallbackId id, void* ptr, TASK_TYPE type);
+  void remove(CallbackId id);
+  void remove(uint8* taskPtr);
+  bool has(const CallbackId &id);
+  TaskPT get(const CallbackId& id);
+  TaskPT get(uint8 * ptr);
+  bool updateNumTasks(uint8 maxRegural, uint8 maxLambda);
+  CallbacksIds ids;
+  uint8 size = 0; // num of items
+  uint8 itemSize = 0; // size -> (id + type + ptr) in bytes 
+
+ private:
+  void shiftMem(uint8* ptr);
+};
+
+class TaskIterator {
+ private:
+  uint8* ptr = nullptr;
+  uint8* end = nullptr;
+  uint8 itemSize = 0;
+
+  TaskPT taskPt{nullptr, TASK_TYPE::empty};  
+
+ public:
+  TaskIterator(uint8* ids) : ptr((uint8*)ids), end((uint8*)ids) {} // constructor for end() 
+  TaskIterator(uint8* ids, uint8 itemSize, uint8 size) { // constructor for begin()
+   // Serial.println("TaskIterator start");
+    this->ptr = ids;  
+    this->end = ids + itemSize * size;
+    this->itemSize = itemSize;
+
+    memcpy(&this->taskPt.pointer, ids + 2, sizeof(void*));
+    this->taskPt.type = getTaskType(ids[1]); // ids[n] 0=id, 1=type, 2-6=ptr
+  //  Serial.printf("TaskType: %d\n", (uint8)this->taskPt.type);
+
+   // Serial.println("TaskIterator start end");
+  }
+
+  TaskPT& operator*() { return taskPt; }
+
+  TaskIterator& operator++() { // todo nullpt
+    //Serial.println("++");
+    ptr+= itemSize;
+    taskPt.type = getTaskType(ptr[1]);
+    memcpy(&this->taskPt.pointer, this->ptr + 2, sizeof(void*)); // todo understand
+
+    if (ptr > end) ptr = end;
+    //Serial.printf("ptr: %u, end: %u\n", (uint32)ptr, (uint32)end);
+    return *this;
+  }
+
+  bool operator!=(const TaskIterator& other) { return ptr != other.ptr; }
+};
+
+class Tasks {
+ public: 
+  Tasks(uint8 maxRegural, uint8 maxLambda);
+  ~Tasks();
+
+  void add(Task &task);
+  void add(TaskLm &task);
+  void remove(CallbackId id);
+  void remove(TaskPT& taskPt);
+  void removeList(TaskPT taskPt[], uint8 len);
+  bool has(Callback callback);
+  bool has(const CallbackId& id);
+  TaskPT get(const CallbackId& id);    // return pointer to Task and type
+  TaskPT get(const Callback callback); // return pointer to Task and type
+  void defragmentation();
+  bool needDefragmentaton(uint8 taskSize);
+  bool isFreeMemForTask(uint8 taskSize);
+  bool canAddTask(uint8 taskSize);
+  bool updateNumTasks(uint8 maxRegural, uint8 maxLambda);
+
+  TaskIterator begin();
+  TaskIterator end();
+
+  uint8* beginTasks = nullptr;
+  uint8* endTasks = nullptr;
+  uint8 size = 0; // num of items
+  size_t sizeTasksInBytes = 0; // current all tasks size
+  size_t maxSizeTasksInBytes = 0; // max available
+  Ids ids;
+};
+
+class PostponedTask { 
  public:
   PostponedTask();
+  PostponedTask(uint8 maxRegural, uint8 maxLambda);
   ~PostponedTask();
 
   uint32 add(uint32 timeMs, Callback callback, bool once = true);
   uint32 add(uint32 timeMs, CallbackLm callback, bool once = true);
-
+  bool remove(CallbackId id);
+  bool remove(Callback callback);
+  bool has(CallbackId id);
+  bool has(Callback callback);
+  bool updateNumTasks(uint8 maxRegural, uint8 maxLambda);
   void tick();
 
-  bool has(Callback callback);
-  bool has(uint32 id);
-  bool remove(Callback callback);
-  bool remove(uint32 id);
-  uint8_t size;
-
-  // template <typename F>
-  // void forEach(F Callback);
+  Tasks tasks;
+  uint8 &size;
 
  private:
-  CallbacksIds ids;
-  uint32 taskId = 1;
-  uint32 minTickTime = 1000;
-  void removeList(Callbacks &list);
-  void removeList(CallbacksIds &list);
-  void updateSize();
-  void updateMinTickTime();
+  void removeList(Callbacks& list, uint8 len);
+  void removeList(CallbacksIds& list, uint8 len);
   void setMinTickTime(const uint32& delay, const uint32& timer,
-                        const uint32& now, uint32& minTime);
-  std::map<Callback, Task> events;
-  std::map<uint32, TaskLm> eventsLm;
+                      const uint32& now, uint32& minTime);
+  void updateMinTickTime();
+
+  Task* regural;
+  TaskLm* lambda;
+
+  CallbackId& getTaskId(TASK_TYPE type);
+  CallbackId taskId = 26;
+  CallbackId taskLmId = 154;
+
+  uint8 numToErase = 0;
+  TaskPT* taskToErase = nullptr;
+
+  uint32 minTickTime = 1000;
 };
 
 extern PostponedTask postponedTask;
